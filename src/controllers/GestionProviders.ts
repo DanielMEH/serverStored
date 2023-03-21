341ae846de224f0f92e8e544fcbf0c3986e73c72
javierhol <javierholguin596@gmail.com>
2023-03-21 18:32:45 -0500

    ADD: providers crud

--- src/controllers/GestionProviders.ts
+++ src/controllers/GestionProviders.ts
@@ -29,11 +29,95 @@ abstract class ManageProviders{
         }
         
     }
-    public async getProviders(){}
-    public async getProvidersId(){}
-    public async putProviders(){}
-    public async deleteProviders(){}
-    public async getProvidersProducts(){}
+    public async getProviders(
+        req: Request, 
+        res: Response, 
+        next: NextFunction
+    ): Promise<Response| Request| any> {
+        try {
+            const providers = await ProviderSchema.find();
+            return res.status(200).json(providers);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+    }
+  
+    public async getProvidersId(
+        req: Request, 
+        res: Response, 
+        next: NextFunction
+    ): Promise<Response| Request| any> {
+        try {
+            const { id } = req.params;
+            const provider = await ProviderSchema.findById(id);
+            return res.status(200).json(provider);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+    }
+
+    public async putProviders(
+        req: Request, 
+        res: Response, 
+        next: NextFunction
+    ): Promise<Response| Request| any> {
+        try {
+            const { id } = req.params;
+            const { name,company, email, phone, address} = req.body;
+            const provider:Provider | any  = {
+                name, company, email, phone, address
+            };
+            const providerUpdated = await ProviderSchema.findByIdAndUpdate(id, provider, {new: true});
+            return res.status(200).json(providerUpdated);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+    }
+
+
+    public async deleteProviders(
+        req: Request, 
+        res: Response, 
+        next: NextFunction
+    ): Promise<Response| Request| any> {
+        try {
+            const { id } = req.params;
+            const providerDeleted = await ProviderSchema.findByIdAndDelete(id);
+            return res.status(200).json(providerDeleted);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+    }
+
+    public async getProvidersProducts(
+        req: Request, 
+        res: Response, 
+        next: NextFunction
+    ): Promise<Response| Request| any> {
+        try {
+            const { id } = req.params;
+            const provider = await ProviderSchema.find({productIdCategory: id});
+            return res.status(200).json(provider);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+    }
+ 
 }
 
 export default ManageProviders;
